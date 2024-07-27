4e5ad85174fc8c2dae1917e022be07ef3efe21ac
Aundre Kerr <hello@aundrekerr.com>
2024-07-27 10:49:05 -0400

    remove old hitboxes, temp fix back to top

--- app/components/BackToTop.tsx
+++ app/components/BackToTop.tsx
@@ -1,15 +1,26 @@
 "use client"
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import { usePathname } from 'next/navigation'
 
 export const BackToTop = () => {
   const backToTopRef = useRef<HTMLDivElement>(null);
   const pathname = usePathname()
+
+  // useEffect(() => {
+  //   if (!backToTopRef.current) return;
+  //   if ((pathname === '/') || (window.scrollY === 0)) {
+  //     backToTopRef.current.className = "hidden"
+  //   } else {
+  //     backToTopRef.current.className = "back-to-top"
+  //   }
+  // }, []);
+
   const scrollBackToTop = () => {
     window.scrollTo(0, 0);
   }
+
   return (
-    <div ref={backToTopRef} onClick={() => scrollBackToTop()} className={`back-to-top ${(pathname === '/' && window.scrollY === 0) ? 'hidden' : ''}`}>
+    <div ref={backToTopRef} onClick={() => scrollBackToTop()} className={`back-to-top`}>
       <span>&#9650;</span>
     </div>
   )
