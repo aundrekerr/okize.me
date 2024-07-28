f5f0336ed360ccc18c8962b3b5bd7a447426a598
Aundre Kerr <hello@aundrekerr.com>
2024-07-28 04:10:43 -0400

    turn it back on, firefox is the problem?

--- app/street-fighter-6/page.tsx
+++ app/street-fighter-6/page.tsx
@@ -16,11 +16,11 @@ export default function SF6Page() {
 
   const [mousePosition, setMousePosition] = React.useState({ x: 0, y: 0 });
   useEffect(() => {
-    // const updateMousePosition = (ev: MouseEvent) => setMousePosition({ x: ev.clientX, y: ev.clientY });
-    // window.addEventListener('mousemove', updateMousePosition);
-    // return () => {
-    //   window.removeEventListener('mousemove', updateMousePosition);
-    // };
+    const updateMousePosition = (ev: MouseEvent) => setMousePosition({ x: ev.clientX, y: ev.clientY });
+    window.addEventListener('mousemove', updateMousePosition);
+    return () => {
+      window.removeEventListener('mousemove', updateMousePosition);
+    };
   }, []);
   
   return (
