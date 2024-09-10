382d6390229bac4002f589fefd238feaa8cdaa40
Aundre Kerr <hello@aundrekerr.com>
2024-09-09 21:54:37 -0400

    A better locals page

--- app/locals/page.tsx
+++ app/locals/page.tsx
@@ -1,15 +1,8 @@
-import Link from "next/link";
-import Image from 'next/image';
-import { motion } from 'framer-motion';
-
 import { LocalsClient } from "@/app/locals/ClientWrapper"
-import { Globe } from "@/app/locals/Globe"
-
 
 export default async function LocalsPage() {
   const localsPromise = async () => (await fetch(process.env.BASE_URL + `/api/locals`)).json();
   const locals = await localsPromise();
-  console.log(locals)
   
   return (
     <LocalsClient locals={locals} />
