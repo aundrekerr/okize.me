078ad69e86cce0675da97189e7e5ec95bf15b7c0
Aundre Kerr <hello@aundrekerr.com>
2024-11-18 18:29:14 -0500

    log country data

--- app/locals/page.tsx
+++ app/locals/page.tsx
@@ -3,7 +3,9 @@ import testData from '@/app/components/locals/testData.json'
 
 export default async function LocalsPage() {
   const countryPromise = await fetch(process.env.BASE_URL + `/api/country`);
+  console.log(countryPromise)
   const countryData = await countryPromise.json();
+  console.log(countryData)
   
   return (
     <Locals 
