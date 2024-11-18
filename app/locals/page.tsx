6db1124c2625a2cd56c80873a5096cfe5c7c7e49
Aundre Kerr <hello@aundrekerr.com>
2024-11-18 18:32:38 -0500

    just send null countrydata

--- app/locals/page.tsx
+++ app/locals/page.tsx
@@ -2,10 +2,11 @@ import { Locals } from "@/app/components/locals"
 import testData from '@/app/components/locals/testData.json'
 
 export default async function LocalsPage() {
-  const countryPromise = await fetch(process.env.BASE_URL + `/api/country`);
-  console.log(countryPromise)
-  const countryData = await countryPromise.json();
-  console.log(countryData)
+  // const countryPromise = await fetch(process.env.BASE_URL + `/api/country`);
+  // console.log(countryPromise)
+  // const countryData = await countryPromise.json();
+  // console.log(countryData)
+  const countryData = null
   
   return (
     <Locals 
